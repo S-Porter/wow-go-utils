98734ec09fa4b545822e8d1720f547df3a00272a
glaaki <dreadpiratesean@gmail.com>
2017-02-05 19:36:33 -0600

    Added read and write to config file.
    Created a dispatcher to send requests to the proper functions.
    Added functions to save and delete chars from the config file.

--- wow.go
+++ wow.go
@@ -11,17 +11,23 @@
 
 package wow
 
-import "fmt"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 type wowConfig struct {
-	installLocation string
-	blizzAPIKey     string
-	characters      []character
+	InstallLocation string      `json:"wowInstall"`
+	BlizzAPIKey     string      `json:"apiKey"`
+	Characters      []character `json:"characters"`
 }
 
 type character struct {
-	Realm string
-	Name  string
+	Realm string `json:"realm"`
+	Name  string `json:"name"`
 }
 
 var config = getConfig()
@@ -29,18 +35,105 @@ var config = getConfig()
 func init() {
 	/* I might not need an init at all, but in here we can:
 	   -set up periodic parsing for data store (or should it just happen when the user searches for something?)
+	*
+	temp, _ := json.Marshal(config)
+	fmt.Printf("config=%+v", string(temp))
+	fmt.Println()
 	*/
-	fmt.Println("wow init. " + config.installLocation + " " + config.blizzAPIKey + " " + config.characters[0].Realm)
+}
+
+func Dispatch(args []string) []byte {
+	if len(args) > 0 {
+		switch args[0] {
+		case "add":
+			return addCharacter(args[1:])
+		case "del":
+			return deleteCharacter(args[1:])
+		default:
+			return errorJSON(errors.New("wowapi, requested api function not found"))
+		}
+	}
+	return errorJSON(errors.New("wow api args were blank"))
 }
 
 func getConfig() wowConfig {
-	return wowConfig{"/path/on/disk", "super-secret-api-key", []character{character{"realm", "char1"}, character{"realm", "char2"}}}
+	raw, fileErr := ioutil.ReadFile("config/config.json")
+	if fileErr != nil {
+		fmt.Println("Error reading file. " + fileErr.Error())
+	}
+
+	var config wowConfig
+	json.Unmarshal(raw, &config)
+	return config
 }
 
-func PackageTest(names []string) string {
-	answer := "zug zug"
-	for _, name := range names {
-		answer = answer + ", " + name
+func addCharacter(charData []string) []byte {
+	if len(charData) != 2 {
+		return errorJSON(errors.New("wrong number of args, character not added"))
 	}
-	return answer
+	charData[0] = strings.Title(charData[0])
+	charData[1] = strings.Title(charData[1])
+	for _, char := range config.Characters {
+		if charData[0] == char.Realm && charData[1] == char.Name {
+			return errorJSON(errors.New("character already exists"))
+		}
+	}
+	config.Characters = append(config.Characters, character{charData[0], charData[1]})
+	if saveConfig() == "failure" {
+		return errorJSON(errors.New("error saving config to file"))
+	}
+	response, err := json.Marshal(struct {
+		Data string `json:"data"`
+	}{"success, character added"})
+	if err != nil {
+		return errorJSON(err)
+	}
+	return response
+}
+
+func deleteCharacter(charData []string) []byte {
+	if len(charData) != 2 {
+		return errorJSON(errors.New("wrong number of args, character not deleted"))
+	}
+	charData[0] = strings.Title(charData[0])
+	charData[1] = strings.Title(charData[1])
+
+	for i, char := range config.Characters {
+		if charData[0] == char.Realm && charData[1] == char.Name {
+			config.Characters = append(config.Characters[:i], config.Characters[i+1:]...)
+			if saveConfig() == "failure" {
+				return errorJSON(errors.New("error saving config to file"))
+			}
+			response, err := json.Marshal(struct {
+				Data string `json:"data"`
+			}{"success, character deleted"})
+			if err != nil {
+				return errorJSON(err)
+			}
+			return response
+		}
+	}
+	return errorJSON(errors.New("character not found"))
+}
+
+func saveConfig() string {
+	data, marshalErr := json.MarshalIndent(config, "", "    ")
+	if marshalErr != nil {
+		return "failure"
+	}
+	writeErr := ioutil.WriteFile("config/config.json", data, 0644)
+	if writeErr != nil {
+		return "failure"
+	}
+	return "success"
+}
+
+func errorJSON(e error) []byte {
+	response, err := json.Marshal(struct {
+		Error string `json:"error"`
+	}{e.Error()})
+	if err != nil {
+		panic("Error encoding the JSON error. Something went horribly wrong.")
+	}
+	return response
 }
