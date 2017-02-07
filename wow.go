1ea4785ee017a5a68da2e37a116c6878c63b8d5a
glaaki <dreadpiratesean@gmail.com>
2017-02-06 20:01:38 -0600

    Split cfg and char data to different files.

--- wow.go
+++ wow.go
@@ -20,17 +20,25 @@ import (
 )
 
 type wowConfig struct {
-	InstallLocation string      `json:"wowInstall"`
-	BlizzAPIKey     string      `json:"apiKey"`
-	Characters      []character `json:"characters"`
+	InstallLocation string `json:"wowInstall"`
+	BlizzAPIKey     string `json:"apiKey"`
 }
-
+type characterData struct {
+	Characters []character `json:"characters"`
+}
 type character struct {
 	Realm string `json:"realm"`
 	Name  string `json:"name"`
+	Items []item `json:"items"`
+}
+type item struct {
+	Name  string `json:"name"`
+	Count int    `json:"count"`
+	//could get an id and pull the item icon from the API/wow folder?
 }
 
-var config = getConfig()
+var config = readConfig()
+var charData = readCharData()
 
 func init() {
 	/* I might not need an init at all, but in here we can:
@@ -40,6 +48,10 @@ func init() {
 	fmt.Printf("config=%+v", string(temp))
 	fmt.Println()
 	*/
+	temp, _ := json.Marshal(charData)
+	fmt.Printf("charData=%+v", string(temp))
+	fmt.Println()
+
 }
 
 // Dispatch takes args from the url and sends them off to the proper fns.
@@ -52,6 +64,8 @@ func Dispatch(args []string) []byte {
 			return deleteCharacter(args[1:])
 		case "listchars":
 			return listCharacters()
+		case "getDataStore":
+			return []byte("hit data store endpoint...")
 		default:
 			return errorJSON(errors.New("wowapi, requested api function not found"))
 		}
@@ -61,26 +75,26 @@ func Dispatch(args []string) []byte {
 
 func listCharacters() []byte {
 	response, err := json.Marshal(struct {
-		Data []character `json:"data"`
-	}{config.Characters})
+		Data []character `json:"data"` //TODO: change this to only marshal name/realm
+	}{charData.Characters})
 	if err != nil {
 		return errorJSON(errors.New("error listing characters"))
 	}
 	return response
 }
 
-func addCharacter(charData []string) []byte {
-	if len(charData) != 2 {
+func addCharacter(newData []string) []byte {
+	if len(newData) != 2 {
 		return errorJSON(errors.New("wrong number of args, character not added"))
 	}
-	newChar := character{strings.Title(charData[0]), strings.Title(charData[1])}
-	for _, configChar := range config.Characters {
+	newChar := character{strings.Title(newData[0]), strings.Title(newData[1]), []item{}}
+	for _, configChar := range charData.Characters {
 		if newChar.Realm == configChar.Realm && newChar.Name == configChar.Name {
 			return errorJSON(errors.New("character already exists"))
 		}
 	}
-	config.Characters = append(config.Characters, newChar)
-	if saveConfig() == "failure" {
+	charData.Characters = append(charData.Characters, newChar)
+	if writeCharData() == "failure" {
 		return errorJSON(errors.New("error saving config to file"))
 	}
 	response, err := json.Marshal(struct {
@@ -92,15 +106,15 @@ func addCharacter(charData []string) []byte {
 	return response
 }
 
-func deleteCharacter(charData []string) []byte {
-	if len(charData) != 2 {
+func deleteCharacter(delData []string) []byte {
+	if len(delData) != 2 {
 		return errorJSON(errors.New("wrong number of args, character not deleted"))
 	}
-	newChar := character{strings.Title(charData[0]), strings.Title(charData[1])}
-	for i, configChar := range config.Characters {
-		if newChar.Realm == configChar.Realm && newChar.Name == configChar.Name {
-			config.Characters = append(config.Characters[:i], config.Characters[i+1:]...)
-			if saveConfig() == "failure" {
+	delChar := character{strings.Title(delData[0]), strings.Title(delData[1]), []item{}}
+	for i, existingChar := range charData.Characters {
+		if delChar.Realm == existingChar.Realm && delChar.Name == existingChar.Name {
+			charData.Characters = append(charData.Characters[:i], charData.Characters[i+1:]...)
+			if writeCharData() == "failure" {
 				return errorJSON(errors.New("error saving config to file"))
 			}
 			response, err := json.Marshal(struct {
@@ -115,23 +129,44 @@ func deleteCharacter(charData []string) []byte {
 	return errorJSON(errors.New("character not found"))
 }
 
-func getConfig() wowConfig {
+func readConfig() wowConfig {
 	raw, fileErr := ioutil.ReadFile("config/config.json")
 	if fileErr != nil {
-		fmt.Println("Error reading file. " + fileErr.Error())
+		fmt.Println("Error reading config file. " + fileErr.Error())
 	}
-
 	var config wowConfig
 	json.Unmarshal(raw, &config)
 	return config
 }
 
-func saveConfig() string {
+func writeConfig() string {
 	data, marshalErr := json.MarshalIndent(config, "", "    ")
 	if marshalErr != nil {
 		return "failure"
 	}
 	writeErr := ioutil.WriteFile("config/config.json", data, 0644)
+	if writeErr != nil {
+		return "failure"
+	}
+	return "success"
+}
+
+func readCharData() characterData {
+	raw, fileErr := ioutil.ReadFile("data/characters.json")
+	if fileErr != nil {
+		fmt.Println("Error reading character data file. " + fileErr.Error())
+	}
+	var data characterData
+	json.Unmarshal(raw, &data)
+	return data
+}
+
+func writeCharData() string {
+	data, marshalErr := json.MarshalIndent(charData, "", "    ")
+	if marshalErr != nil {
+		return "failure"
+	}
+	writeErr := ioutil.WriteFile("data/characters.json", data, 0644)
 	if writeErr != nil {
 		return "failure"
 	}
